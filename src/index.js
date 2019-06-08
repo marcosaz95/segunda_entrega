9a11bf88c283ea7c506d5dd175419422097cd91c
marcosaz95 <marcosaz95@gmail.com>
2019-06-08 08:27:44 -0500

    bootstrap and initial configuration

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// Initialize express
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+
+//Register all the folders / libraries
+const publicDirectory = path.join(__dirname, '../public');
+const partialsDirectory = path.join(__dirname, '../partials');
+const dirNode_modules = path.join(__dirname , '../node_modules')
+app.use(express.static(publicDirectory));
+app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
+app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
+app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
+app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
+
+hbs.registerPartials(partialsDirectory);
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+
+app.listen(3000, () => {
+    console.log('Corriendo node en el puerto 3000');
+});
